3c7d9005f549d8cc77871c1ddb9ec33c7ff3c9b0
Glenn Posadas <hello@glennvon.com>
2021-01-21 23:58:48 +0800

    working sliding content.

---
+++ SwipeLetters/Views/ContentViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ContentViewController.swift
+//  SwipeLetters
+//
+//  Created by Glenn Posadas on 1/21/21.
+//
+
+import AVFoundation
+import UIKit
+
+class ContentViewController: BaseViewController {
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var label: UILabel!
+    var text: String?
+    
+    // MARK: - Overrides
+    // MARK: Functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        label.text = text
+    }
+    
+    @IBAction func labelTapped(_ sender: UITapGestureRecognizer) {
+        guard let lastChar = text?.last else { return }
+        let synthesizer = AVSpeechSynthesizer()
+        let utterance = AVSpeechUtterance(string: String(lastChar))
+        synthesizer.speak(utterance)
+    }
+}
